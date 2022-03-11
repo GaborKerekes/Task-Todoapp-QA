9401fb34c0db0180900f6dbe2ab00c7224331026
Gabor Kerekes <gksrt4@outlook.com>
2022-03-11 12:43:06 +0000

    firt commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#Lots of blood, sweat and tears but this basic app seems working finally.
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os 
